38937cadb30872c4600bd53dc286e928518a877c
Jose Alberto Velasquez <josevelasquez3064@gmail.com>
2021-06-07 00:28:47 -0600

    Primer Commit -m

---
+++ src/App.js
@@ -0,0 +1,20 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">   
+        <nav className ="navbar navbar-dark bg-dark">
+          <a href="#">
+            Hola Software 
+          </a>
+        </nav>
+        
+        <img src={logo} className="App-logo" alt="logo" />     
+      
+    </div>
+  );
+}
+
+export default App;
