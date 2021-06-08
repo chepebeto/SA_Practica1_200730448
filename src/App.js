7c39355fdc16622ffb52c21d9d8f3b9160bf345b
Jose Alberto Velasquez <josevelasquez3064@gmail.com>
2021-06-07 19:48:33 -0600

    Cambios para prueba de integración y despliegue continúo

--- src/App.js
+++ src/App.js
@@ -5,12 +5,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">           
-          <h1> Hola Software </h1>
+          <h1> Hello World </h1>
           <h2> Practica 1</h2>
           <h5> José Alberto Velásquez Orozco - 200730448</h5>
     </div>
   );
 }
-
 export default App;
 
