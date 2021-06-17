1a4f1a1ff9ac90f34849b147d361a3ac018b57a8
Jose Alberto Velasquez <josevelasquez3064@gmail.com>
2021-06-16 23:14:25 -0600

    Cambio en pantalla

--- fabric/frontend.py
+++ fabric/frontend.py
@@ -5,6 +5,6 @@
 import paramiko
 client = paramiko.SSHClient()
 
-ssh.connect(hostname='34.136.129.108', username='chepebeto_1590', password='123456')
-shh.connect.run("pwd")
+client.connect(hostname='34.136.129.108', username='chepebeto_1590', password='123456')
+client.connect.run("pwd")
 
