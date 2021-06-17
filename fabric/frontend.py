267acd218edd32b0c8600a5af2ef9808a92c87f7
Jose Alberto Velasquez <josevelasquez3064@gmail.com>
2021-06-16 23:12:24 -0600

    Cambio en pantalla

--- fabric/frontend.py
+++ fabric/frontend.py
@@ -1,3 +1,10 @@
-from fabric import Connection
-connection = Connection(host="chepebeto_1590@34.136.129.108", connect_kwargs = {"password":"123456"})
-connection.run("sudo adduser jenkins")
+#from fabric import Connection
+#connection = Connection(host="chepebeto_1590@34.136.129.108", connect_kwargs = {"password":"123456"})
+#connection.run("sudo adduser jenkins")
+
+import paramiko
+client = paramiko.SSHClient()
+
+ssh.connect(hostname='34.136.129.108', username='chepebeto_1590', password='123456')
+shh.connect.run("pwd")
+
