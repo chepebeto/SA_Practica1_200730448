3e1a8dab99cae3752b1af9ef2edc603d440ca64a
Jose Alberto Velasquez <josevelasquez3064@gmail.com>
2021-06-16 22:46:40 -0600

    Cambio en pantalla

--- fabric/frontend.py
+++ fabric/frontend.py
@@ -1,3 +1,3 @@
 from fabric import Connection
-#connection = Connection(host="chepebeto_1590@34.136.129.108", connect_kwargs = {"password":"123456"})
+connection = Connection(host="chepebeto_1590@34.136.129.108", connect_kwargs = {"password":"123456"})
 #connection.run("whoami")
