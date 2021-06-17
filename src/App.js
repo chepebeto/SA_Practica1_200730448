72aac416f2dce5efa7a003152effff8b315845fb
Jose Alberto Velasquez <josevelasquez3064@gmail.com>
2021-06-16 21:56:58 -0600

    Cambio en pantalla

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">           
-          <h1> Hello World </h1>
+          <h1> Hola Software </h1>
           <h2> Practica 1</h2>
           <h5> José Alberto Velásquez Orozco - 200730448</h5>
     </div>
