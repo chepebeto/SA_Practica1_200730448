0620d713f25d8704ae0d709acb97589cf8ac3c19
Jose Alberto Velasquez <josevelasquez3064@gmail.com>
2021-06-16 23:47:09 -0600

    Cambio en pantalla

--- fabric/frontend.py
+++ fabric/frontend.py
@@ -1,12 +1,4 @@
 from fabric import Connection
-#connection = Connection(host="chepebeto_1590@34.136.129.108", connect_kwargs = {"password":"123456"})
+connection = Connection(host="chepebeto_1590@34.136.129.108", connect_kwargs = {"password":"123456"})
 
-connection = Connection(
-    host="34.136.129.108",
-    user="chepebeto_1590",
-    connect_kwargs={
-        "key_filename": "/home/chepebeto_1590/.ssh/id_rsa.pub",
-    }   
-)
 
-connection.run("pwd")
