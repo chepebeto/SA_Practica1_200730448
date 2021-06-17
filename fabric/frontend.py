37aef9b238a1ffd52fcbf8e3751ceefa4ce09734
Jose Alberto Velasquez <josevelasquez3064@gmail.com>
2021-06-16 23:35:45 -0600

    Cambio en pantalla

--- fabric/frontend.py
+++ fabric/frontend.py
@@ -5,7 +5,7 @@ connection = Connection(
     host="34.136.129.108",
     user="chepebeto_1590",
     connect_kwargs={
-        "key_filename": "~/.ssh/id_rsa.pub",
+        "key_filename": "/home/chepebeto_1590/.ssh/id_rsa.pub",
     }   
 )
 
